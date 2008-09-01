653b95675b53a941bce51553b127f95aad525eb1
MatthewRudy <MatthewRudyJacobs@gmail.com>
2008-09-02 00:38:11 +0100

    slide #4

---
+++ slides/4.rb
@@ -0,0 +1,15 @@
+>> rake spec
+(in /home/matthewrudy/code/faily)
+rake spec:doc
+F.
+ 
+1)
+'Something some_method should return :something' FAILED
+expected: :something,
+     got: :something_else (using ==)
+./spec/models/something_spec.rb:17:
+ 
+Finished in 0.466476 seconds
+ 
+2 examples, 1 failure
+rake aborted! 
