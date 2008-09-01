60644d04ef71524a38ca39c4c37680716e896561
MatthewRudy <MatthewRudyJacobs@gmail.com>
2008-09-02 00:33:14 +0100

    slide 1

---
+++ slides/1.rb
@@ -0,0 +1,21 @@
+<<-COMMENT
+
+  We have an app....
+
+  Let's create a new model
+
+COMMENT
+
+
+
+  >> ruby script/generate rspec_model Something with_a_name:string
+
+      exists  app/models/
+      exists  spec/models/
+      exists  spec/fixtures/
+      create  app/models/something.rb
+      create  spec/models/something_spec.rb
+   identical  spec/fixtures/somethings.yml
+      exists  db/migrate
+      create  db/migrate/20080901233141_create_somethings.rb
+
